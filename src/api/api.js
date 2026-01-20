3f707ee6be21303761d36567b0af09c7df6fcde2
muvvadhanush <68426077+muvvadhanush@users.noreply.github.com>
2026-01-20 15:14:24 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://idea-management-server.onrender.com';
 
 export const api = {
     createIdea: (data) => axios.post(`${API_URL}/ideas`, data),
